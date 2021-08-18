e353d4197de61139c0bf067cd06178332817a554
jkbs487 <416578078@qq.com>
2021-08-18 22:07:48 +0800

    refactor: using C++ style

--- src/simple.cc
+++ src/simple.cc
@@ -1,101 +1,149 @@
 #include "TCPServer.h"
+#include "EventLoop.h"
 
 #include <ctime>
 #include <cstdio>
 
-void echo(TCPConnection *conn, std::string buffer)
-{
-    printf("echo %ld bytes\n", buffer.size());
-
-    conn->send(buffer);
-}
-
-void discard(TCPConnection *conn, std::string buffer)
-{
-    printf("discards %ld bytes\n", buffer.size());
-}
-
-void daytime(TCPConnection *conn)
-{
-    time_t rawtime;
-    struct tm *info;
-    char buffer[80];
-
-    time(&rawtime);    
-    info = localtime(&rawtime);
-    strftime(buffer, 80, "%Y-%m-%d %H:%M:%S", info);
-    if (conn->connected()) {
-        conn->send(std::string(buffer));
-        conn->shutdown();
+class EchoServer {
+public:
+    EchoServer(std::string host, uint16_t port, EventLoop *eventLoop): server(host, port, eventLoop) {
+        server.setMessageCallback(std::bind(&EchoServer::onMessage, this, std::placeholders::_1, std::placeholders::_2));
     }
-}
-
-void time_(TCPConnection *conn)
-{
-    time_t now = ::time(NULL);
-
-    int32_t be32 = htobe32(static_cast<int32_t>(now));
-    if (conn->connected()) {
-        conn->send(std::to_string(be32));
-        conn->shutdown();
+    void start() {
+        server.start();
     }
-}
-
-std::string message;
-
-void chargen(TCPConnection *conn)
-{
-    if (conn->connected()) {
-        conn->send(message);
+private:
+    TCPServer server;
+    void onMessage(TCPConnection *conn, std::string buffer) {
+        printf("echo %ld bytes\n", buffer.size());
+        conn->send(buffer);
     }
-}
+};
 
-void onWriteComplete(TCPConnection *conn)
-{
-    if (conn->connected()) {
-        conn->send(message);
+class DiscardServer {
+public:
+    DiscardServer(std::string host, uint16_t port, EventLoop *eventLoop): server(host, port, eventLoop) {
+        server.setMessageCallback(std::bind(&DiscardServer::onMessage, this, std::placeholders::_1, std::placeholders::_2));
     }
-}
+    void start() {
+        server.start();
+    }
+private:
+    TCPServer server;
+    void onMessage(TCPConnection *conn, std::string buffer)
+    {
+        printf("discards %ld bytes\n", buffer.size());
+    }
+};
+
+class DaytimeServer {
+public:
+    DaytimeServer(std::string host, uint16_t port, EventLoop *eventLoop): server(host, port, eventLoop) {
+        server.setConnectionCallback(std::bind(&DaytimeServer::onConnection, this, std::placeholders::_1));
+    }
+    void start() {
+        server.start();
+    }
+private:
+    TCPServer server;
+    void onConnection(TCPConnection *conn)
+    {
+        time_t rawtime;
+        struct tm *info;
+        char buffer[80];
+
+        time(&rawtime);    
+        info = localtime(&rawtime);
+        strftime(buffer, 80, "%Y-%m-%d %H:%M:%S", info);
+        if (conn->connected()) {
+            conn->send(std::string(buffer));
+            conn->shutdown();
+        }
+    }
+};
+
+class TimeServer {
+public:
+    TimeServer(std::string host, uint16_t port, EventLoop *eventLoop): server(host, port, eventLoop) {
+        server.setConnectionCallback(std::bind(&TimeServer::onConnection, this, std::placeholders::_1));
+    }
+    void start() {
+        server.start();
+    }
+private:
+    TCPServer server;
+    void onConnection(TCPConnection *conn)
+    {
+        time_t now = ::time(NULL);
+
+        int32_t be32 = htobe32(static_cast<int32_t>(now));
+        if (conn->connected()) {
+            conn->send(std::to_string(be32));
+            conn->shutdown();
+        }
+    }
+};
+
+class ChargenServer {
+public:
+    ChargenServer(std::string host, uint16_t port, EventLoop *eventLoop): server(host, port, eventLoop) {
+        createMessage();
+        server.setConnectionCallback(std::bind(&ChargenServer::onConnection, this, std::placeholders::_1));
+        server.setWriteCompleteCallback(std::bind(&ChargenServer::onWriteComplete, this, std::placeholders::_1));
+    }
+    void start() {
+        server.start();
+    }
+private:
+    TCPServer server;
+    std::string message;
+    void createMessage() {
+        std::string line;
+        for (int i = 33; i < 127; ++i)
+        {
+        line.push_back(char(i));
+        }
+        line += line;
+
+        for (size_t i = 0; i < 127-33; ++i)
+        {
+        message += line.substr(i, 72) + '\n';
+        }
+    }
+    void onConnection(TCPConnection *conn)
+    {
+        if (conn->connected()) {
+            conn->send(message);
+        }
+    }
+    void onWriteComplete(TCPConnection *conn)
+    {
+        if (conn->connected()) {
+            conn->send(message);
+        }
+    }
+};
 
 int main(int argc, char *argv[])
 {
-    if (argc != 2) {
-        printf("Usage: %s [service]\n", argv[0]);
-        ::exit(1);
-    }
-/*
-    std::string line;
-    for (int i = 33; i < 127; ++i)
-    {
-      line.push_back(char(i));
-    }
-    line += line;
+    EventLoop eventLoop;
 
-    for (size_t i = 0; i < 127-33; ++i)
-    {
-      message += line.substr(i, 72) + '\n';
-    }
-*/
+    EchoServer echoServer("0.0.0.0", 10001, &eventLoop);
+    //echoServer.start();
 
-    TCPServer tcpserver("0.0.0.0", 12345);
+    DiscardServer discardServer("0.0.0.0", 10002, &eventLoop);
+    discardServer.start();
 
-    if (!strcmp(argv[1], "discard")) {
-        tcpserver.setMessageCallback(discard);
-    } else if (!strcmp(argv[1], "daytime")) {
-        tcpserver.setConnectionCallback(daytime);
-    } else if (!strcmp(argv[1], "time")) {
-        tcpserver.setConnectionCallback(time_);
-    } else if (!strcmp(argv[1], "echo")) {
-        tcpserver.setMessageCallback(echo);
-    } else if (!strcmp(argv[1], "chargen")) {
-        tcpserver.setConnectionCallback(chargen);
-        tcpserver.setMessageCallback(discard);
-        tcpserver.setWriteCompleteCallback(onWriteComplete);    
-    } else {
-        printf("server error! [discard daytime time echo chargen]\n");
-        ::exit(1);
-    }
+    DaytimeServer daytimeServer("0.0.0.0", 10003, &eventLoop);
+    //daytimeServer.start();
 
-    tcpserver.start();
+    TimeServer timeServer("0.0.0.0", 10004, &eventLoop);
+    //timeServer.start();
+
+    ChargenServer chargenServer("0.0.0.0", 10005, &eventLoop);
+    chargenServer.start();
+
+    eventLoop.loop();
+
     return 0;
 }
