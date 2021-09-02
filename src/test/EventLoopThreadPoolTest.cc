46f693f28abebebdfd7406c22b51310320058d31
jkbs487 <416578078@qq.com>
2021-09-02 07:53:48 -0700

    feat: add unittest case, all tests passed

---
+++ src/test/EventLoopThreadPoolTest.cc
@@ -0,0 +1,64 @@
+#include "EventLoopThreadPool.h"
+#include "EventLoop.h"
+
+#include <stdio.h>
+#include <unistd.h>
+#include <assert.h>
+#include <iostream>
+
+void print(EventLoop* p = NULL)
+{
+    std::cout << "main(): print: pid = " << getpid() << ", tid = " << std::this_thread::get_id() << ", loop = " << p << std::endl;
+}
+
+void init(EventLoop* p)
+{
+    std::cout << "init(): print: pid = " << getpid() << ", tid = " << std::this_thread::get_id() << ", loop = " << p << std::endl;
+}
+
+int main()
+{
+  print();
+
+  EventLoop loop;
+  //loop.runAfter(11, std::bind(&EventLoop::quit, &loop));
+
+  {
+    printf("Single thread %p:\n", &loop);
+    EventLoopThreadPool model(&loop);
+    model.setThreadNum(0);
+    model.start();
+    assert(model.getNextLoop() == &loop);
+    assert(model.getNextLoop() == &loop);
+    assert(model.getNextLoop() == &loop);
+  }
+
+  {
+    printf("Another thread:\n");
+    EventLoopThreadPool model(&loop);
+    model.setThreadNum(1);
+    model.start();
+    EventLoop* nextLoop = model.getNextLoop();
+    nextLoop->runTask(std::bind(print, nextLoop));
+    assert(nextLoop != &loop);
+    assert(nextLoop == model.getNextLoop());
+    assert(nextLoop == model.getNextLoop());
+    ::sleep(3);
+  }
+
+  {
+    printf("Three threads:\n");
+    EventLoopThreadPool model(&loop);
+    model.setThreadNum(3);
+    model.start();
+    EventLoop* nextLoop = model.getNextLoop();
+    nextLoop->runTask(std::bind(print, nextLoop));
+    assert(nextLoop != &loop);
+    assert(nextLoop != model.getNextLoop());
+    assert(nextLoop != model.getNextLoop());
+    assert(nextLoop == model.getNextLoop());
+  }
+
+  loop.loop();
+}
+
---
+++ src/test/EventLoopThreadTest.cc
@@ -0,0 +1,44 @@
+#include "EventLoopThread.h"
+#include "EventLoop.h"
+
+#include <thread>
+#include <chrono>
+#include <iostream>
+#include <unistd.h>
+
+void print(EventLoop* p = NULL)
+{
+    std::cout << "print: pid = " << getpid() << ", tid = " << std::this_thread::get_id() << ", loop = " << p << std::endl;
+}
+
+void quit(EventLoop* p)
+{
+    print(p);
+    p->quit();
+}
+
+int main()
+{
+    print();
+
+    {
+        EventLoopThread thr1;  // never start
+    }
+
+    {
+        // dtor calls quit()
+        EventLoopThread thr2;
+        EventLoop* loop = thr2.startLoop();
+        loop->runTask(std::bind(print, loop));
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    }
+
+    {
+        // quit() before dtor
+        EventLoopThread thr3;
+        EventLoop* loop = thr3.startLoop();
+        loop->runTask(std::bind(quit, loop));
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    }
+}
+
