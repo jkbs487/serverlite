760d9af9f5354b9a67e37e5475cee5ac532a5a46
jkbs487 <416578078@qq.com>
2021-12-28 05:36:39 -0800

    feat: add thoughtout statistic in ChargenServer

--- examples/simple.cc
+++ examples/simple.cc
@@ -1,33 +1,39 @@
 #include "TCPServer.h"
 #include "EventLoop.h"
 #include "EventLoopThread.h"
+#include "Logger.h"
 
 #include <ctime>
+#include <sys/time.h>
 #include <cstdio>
-#include <iostream>
+#include <thread>
+
+#define TIME_SUB_MS(tv1, tv2)  ((tv1.tv_sec - tv2.tv_sec) * 1000 + (tv1.tv_usec - tv2.tv_usec) / 1000)
 
 class EchoServer {
 public:
-    EchoServer(std::string host, uint16_t port, EventLoop *eventLoop): server(host, port, eventLoop) {
+    EchoServer(std::string host, uint16_t port, EventLoop *eventLoop): server(host, port, eventLoop, "EchoServer") {
         server.setMessageCallback(std::bind(&EchoServer::onMessage, this, std::placeholders::_1, std::placeholders::_2));
     }
     void start() {
+        //server.setThreadNum(4);
         server.start();
     }
 private:
     TCPServer server;
     void onMessage(const TCPConnectionPtr& conn, std::string& buffer) {
-        printf("echo %ld bytes\n", buffer.size());
+        LOG_INFO << "echo " << buffer.size() << " bytes";
         conn->send(std::move(buffer));
     }
 };
 
 class DiscardServer {
 public:
-    DiscardServer(std::string host, uint16_t port, EventLoop *eventLoop): server(host, port, eventLoop) {
+    DiscardServer(std::string host, uint16_t port, EventLoop *eventLoop): server(host, port, eventLoop, "DiscardServer") {
         server.setMessageCallback(std::bind(&DiscardServer::onMessage, this, std::placeholders::_1, std::placeholders::_2));
     }
     void start() {
+        //server.setThreadNum(4);
         server.start();
     }
 private:
@@ -36,13 +42,13 @@ private:
     {
         std::string discard;
         discard.swap(buffer);
-        printf("discards %ld bytes\n", discard.size());
+        LOG_INFO << "discards " << discard.size() << " bytes";
     }
 };
 
 class DaytimeServer {
 public:
-    DaytimeServer(std::string host, uint16_t port, EventLoop *eventLoop): server(host, port, eventLoop) {
+    DaytimeServer(std::string host, uint16_t port, EventLoop *eventLoop): server(host, port, eventLoop, "DaytimeServer") {
         server.setConnectionCallback(std::bind(&DaytimeServer::onConnection, this, std::placeholders::_1));
     }
     void start() {
@@ -68,7 +74,7 @@ private:
 
 class TimeServer {
 public:
-    TimeServer(std::string host, uint16_t port, EventLoop *eventLoop): server(host, port, eventLoop) {
+    TimeServer(std::string host, uint16_t port, EventLoop *eventLoop): server(host, port, eventLoop, "TimeServer") {
         server.setConnectionCallback(std::bind(&TimeServer::onConnection, this, std::placeholders::_1));
     }
     void start() {
@@ -90,18 +96,31 @@ private:
 
 class ChargenServer {
 public:
-    ChargenServer(std::string host, uint16_t port, EventLoop *eventLoop): server(host, port, eventLoop) {
+    ChargenServer(std::string host, uint16_t port, EventLoop *loop)
+        : server_(host, port, loop, "ChargenServer"),
+        startTime_(clock()),
+        messageCount_(0)
+    {
         createMessage();
-        server.setConnectionCallback(std::bind(&ChargenServer::onConnection, this, std::placeholders::_1));
-        server.setWriteCompleteCallback(std::bind(&ChargenServer::onWriteComplete, this, std::placeholders::_1));
+        server_.setConnectionCallback(
+            std::bind(&ChargenServer::onConnection, this, std::placeholders::_1));
+        server_.setWriteCompleteCallback(
+            std::bind(&ChargenServer::onWriteComplete, this, std::placeholders::_1));
+        loop->runEvery(3.0, std::bind(&ChargenServer::printThroughput, this));
+        Logger::setLogLevel(Logger::DEBUG);
     }
     void start() {
-        server.start();
+        //server.setThreadNum(4);
+        server_.start();
     }
 private:
-    TCPServer server;
-    std::string message;
-    void createMessage() {
+    TCPServer server_;
+    std::string message_;
+    clock_t startTime_;
+    long messageCount_;
+
+    void createMessage() 
+    {
         std::string line;
         for (int i = 33; i < 127; ++i)
         {
@@ -111,27 +130,38 @@ private:
 
         for (size_t i = 0; i < 127-33; ++i)
         {
-        message += line.substr(i, 72) + '\n';
+        message_ += line.substr(i, 72) + '\n';
         }
     }
-    void onConnection(const TCPConnectionPtr& conn)
+
+    void onConnection(const TCPConnectionPtr& conn) 
     {
         if (conn->connected()) {
-            conn->send(message);
+            conn->send(message_);
         }
     }
-    void onWriteComplete(const TCPConnectionPtr& conn)
+
+    void onWriteComplete(const TCPConnectionPtr& conn) 
     {
+        messageCount_ += message_.size();
         if (conn->connected()) {
-            conn->send(message);
+            conn->send(message_);
         }
+    }
+
+    void printThroughput()
+    {
+        clock_t endTime = clock();
+        double interval = static_cast<double>(endTime - startTime_) / CLOCKS_PER_SEC;
+        printf("%4.3f MiB/s\n", static_cast<double>(messageCount_)/interval/1024/1024);
+        messageCount_ = 0;
+        startTime_ = endTime;
     }
 };
 
 int main(int argc, char *argv[])
 {
     EventLoop eventLoop;
-    EventLoopThread eventLoopThread;
 
     EchoServer echoServer("0.0.0.0", 10001, &eventLoop);
     echoServer.start();
